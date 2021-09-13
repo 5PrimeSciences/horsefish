e782ba351039d3453000a716c4e596d81c03a429
Emma Pierce-Hoffman <epierceh@broadinstitute.org>
2021-09-13 17:52:25 -0400

    retry lambda function on_error after importlib.reload

--- scripts/mop_workspace/mop_workspace.py
+++ scripts/mop_workspace/mop_workspace.py
@@ -187,9 +187,10 @@ def delete_files(bucket_name, files_to_delete, verbose):
     storage_client = storage.Client()
 
     bucket = storage_client.bucket(bucket_name)
-    blobs = [bucket.blob(blob_name) for blob_name in blob_names if storage.Blob(bucket=bucket_name, name=blob_name).exists()]
-    if verbose:
-        print(f"Preparing to delete {len(blobs)} files that exist from bucket {bucket_name}")
+    blobs = [bucket.blob(blob_name) for blob_name in blob_names]
+    # blobs = [bucket.blob(blob_name) for blob_name in blob_names if storage.Blob(bucket=bucket_name, name=blob_name).exists()]
+    # if verbose:
+    #     print(f"Preparing to delete {len(blobs)} files that exist from bucket {bucket_name}")
 
     # storage_client.close()
 
